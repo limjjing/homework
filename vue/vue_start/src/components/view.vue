a7bd98c4fb9c3c3e7872537539e1040588d93721
home_lim <limjjing@naver.com>
2019-04-25 23:23:47 +0900

    view~

--- vue/vue_start/src/components/view.vue
+++ vue/vue_start/src/components/view.vue
@@ -36,6 +36,12 @@
 									<span class="">저장</span>
 								</button>
 							</li>
+							<li>
+								<button type="button">
+									<span class="tu_ic"></span>
+									<span class="">설정</span>
+								</button>
+							</li>
 						</ul>
 					</div>
 				</div>
@@ -43,7 +49,7 @@
 				<div class="v_info">
 					<div class="ch_line">
 						<div class="ch_user">
-							<div class="cu_thumb"></div>
+							<a href="javascript:void(0);"><div class="cu_thumb"></div></a>
 							<div class="cu_info">
 								<p class="ci_1"><a href="javascript:void(0);">치킨먹고가</a></p>
 								<p class="ci_2">게시일: 2019. 4. 19.</p>
@@ -62,7 +68,7 @@
 
 						블랙핑크, LA 콘서트 뜨거운 현장 분위기, 해리 스타일스도 관람해</p>
 						<div class="vt_bb">
-							<span class="">카테고리</span>
+							<span class="vb_c1">카테고리</span>
 							<a href="javascript:void(0);">엔터테인먼트</a>
 						</div>
 					</div>
@@ -103,6 +109,54 @@
 									<div class="comm_view"><button type="button">답글보기</button></div>
 								</div>
 							</li>
+							<li>
+								<div class="cm_user"></div>
+								<div class="cm_tbox">
+									<p class="nick">
+										<span class="n_1">ii ll</span>
+										<span class="n_2">6일 전</span>
+									</p>
+									<p class="cm_t">퍼랠 윌리엄스, 해리 스타일스, 디제이 스네이크, 레이니, 베니 블랑코 등 유명 인사들 많이 와서 신기</p>
+									<div class="cm_util">
+										<span class="cu_1"><span class=""></span> 46</span>
+										<span class="cu_2"><span class=""></span> 10</span>
+										<button type="button">답글</button>
+									</div>
+									<div class="comm_view"><button type="button">답글보기</button></div>
+								</div>
+							</li>
+							<li>
+								<div class="cm_user"></div>
+								<div class="cm_tbox">
+									<p class="nick">
+										<span class="n_1">이안</span>
+										<span class="n_2">6일 전</span>
+									</p>
+									<p class="cm_t">텅텅콘이라 날조했던 것들 또 ㅂㄷㅂㄷ거리겠네 ㅋㅋㅋㅋㅋㅋㅋㅋㅋ갓블핑</p>
+									<div class="cm_util">
+										<span class="cu_1"><span class=""></span> 46</span>
+										<span class="cu_2"><span class=""></span> 10</span>
+										<button type="button">답글</button>
+									</div>
+									<div class="comm_view"><button type="button">답글보기</button></div>
+								</div>
+							</li>
+							<li>
+								<div class="cm_user"></div>
+								<div class="cm_tbox">
+									<p class="nick">
+										<span class="n_1">Yeon ‘s</span>
+										<span class="n_2">5일 전</span>
+									</p>
+									<p class="cm_t">무사히 잘끝내서 그것만으로도 다행이네용ㅜㅜ 블핑이들 수고많았고 편히 쉬셨으면...❤️</p>
+									<div class="cm_util">
+										<span class="cu_1"><span class=""></span> 46</span>
+										<span class="cu_2"><span class=""></span> 10</span>
+										<button type="button">답글</button>
+									</div>
+									<div class="comm_view"><button type="button">답글보기</button></div>
+								</div>
+							</li>
 						</ul>
 					</div>
 				</div>
@@ -111,7 +165,213 @@
 			<div class="v_aside">
 				<div class="va_banner"></div>
 
-				<div class="nb_list"></div>
+				<div class="nb_list">
+					<ul>
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/e7ZMZ0uyewg/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&amp;rs=AOn4CLAtMiUeeZEYoVhakmV_LG8Sy2obFw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">관련 재생목록 - 블랙핑크] LA 콘서트 열광하는 현장 분위기, 해리 스타일스도 관람</p>
+									<p class="nt_2">YouTube</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/6uN-8zoqZ3E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDomCvIwd3xPJLCaZlc6XQyo_ph7Q" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">들을수록 똑같은 환희 모창능력자 박민규의 '남자답게' ♪ -히든싱어3 15회</p>
+									<p class="nt_2">JTBC Entertainment</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/RrrQbAo1M14/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCFMaBUdD2W9c1NKoXK-wXdINvTZg" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">세친구 레전드 정력왕 정웅인 [레전드 캐릭터]</p>
+									<p class="nt_2">레전드캐릭터</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/d8BFubjIKn8/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBTlUNkizqKRrCHJs0SCuwBnOmFWw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">블랙핑크가 최단기간 월드 클래스로 성장한 이유는? [아이돌 알려줌] 블랙핑크 편 Explained BLACKPINK</p>
+									<p class="nt_2">알려줌 팬질 (ALZ Fanzeel)</p>
+									<p class="nt_3">조회수 29만회</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/e7ZMZ0uyewg/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&amp;rs=AOn4CLAtMiUeeZEYoVhakmV_LG8Sy2obFw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">관련 재생목록 - 블랙핑크] LA 콘서트 열광하는 현장 분위기, 해리 스타일스도 관람</p>
+									<p class="nt_2">YouTube</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/6uN-8zoqZ3E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDomCvIwd3xPJLCaZlc6XQyo_ph7Q" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">들을수록 똑같은 환희 모창능력자 박민규의 '남자답게' ♪ -히든싱어3 15회</p>
+									<p class="nt_2">JTBC Entertainment</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/RrrQbAo1M14/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCFMaBUdD2W9c1NKoXK-wXdINvTZg" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">세친구 레전드 정력왕 정웅인 [레전드 캐릭터]</p>
+									<p class="nt_2">레전드캐릭터</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/d8BFubjIKn8/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBTlUNkizqKRrCHJs0SCuwBnOmFWw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">블랙핑크가 최단기간 월드 클래스로 성장한 이유는? [아이돌 알려줌] 블랙핑크 편 Explained BLACKPINK</p>
+									<p class="nt_2">알려줌 팬질 (ALZ Fanzeel)</p>
+									<p class="nt_3">조회수 29만회</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/e7ZMZ0uyewg/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&amp;rs=AOn4CLAtMiUeeZEYoVhakmV_LG8Sy2obFw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">관련 재생목록 - 블랙핑크] LA 콘서트 열광하는 현장 분위기, 해리 스타일스도 관람</p>
+									<p class="nt_2">YouTube</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/6uN-8zoqZ3E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDomCvIwd3xPJLCaZlc6XQyo_ph7Q" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">들을수록 똑같은 환희 모창능력자 박민규의 '남자답게' ♪ -히든싱어3 15회</p>
+									<p class="nt_2">JTBC Entertainment</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/RrrQbAo1M14/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCFMaBUdD2W9c1NKoXK-wXdINvTZg" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">세친구 레전드 정력왕 정웅인 [레전드 캐릭터]</p>
+									<p class="nt_2">레전드캐릭터</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/d8BFubjIKn8/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBTlUNkizqKRrCHJs0SCuwBnOmFWw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">블랙핑크가 최단기간 월드 클래스로 성장한 이유는? [아이돌 알려줌] 블랙핑크 편 Explained BLACKPINK</p>
+									<p class="nt_2">알려줌 팬질 (ALZ Fanzeel)</p>
+									<p class="nt_3">조회수 29만회</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/e7ZMZ0uyewg/hqdefault.jpg?sqp=-oaymwEWCKgBEF5IWvKriqkDCQgBFQAAiEIYAQ==&amp;rs=AOn4CLAtMiUeeZEYoVhakmV_LG8Sy2obFw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">관련 재생목록 - 블랙핑크] LA 콘서트 열광하는 현장 분위기, 해리 스타일스도 관람</p>
+									<p class="nt_2">YouTube</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/6uN-8zoqZ3E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLDomCvIwd3xPJLCaZlc6XQyo_ph7Q" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">들을수록 똑같은 환희 모창능력자 박민규의 '남자답게' ♪ -히든싱어3 15회</p>
+									<p class="nt_2">JTBC Entertainment</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/RrrQbAo1M14/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCFMaBUdD2W9c1NKoXK-wXdINvTZg" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">세친구 레전드 정력왕 정웅인 [레전드 캐릭터]</p>
+									<p class="nt_2">레전드캐릭터</p>
+									<p class="nt_3">맞춤 동영상</p>
+								</div>
+							</a>
+						</li>
+
+						<li>
+							<a href="javascript:void(0);">
+								<div class="nb_thumb">
+									<img src="https://i.ytimg.com/vi/d8BFubjIKn8/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBTlUNkizqKRrCHJs0SCuwBnOmFWw" alt="">
+								</div>
+								<div class="nb_text">
+									<p class="nt_1">블랙핑크가 최단기간 월드 클래스로 성장한 이유는? [아이돌 알려줌] 블랙핑크 편 Explained BLACKPINK</p>
+									<p class="nt_2">알려줌 팬질 (ALZ Fanzeel)</p>
+									<p class="nt_3">조회수 29만회</p>
+								</div>
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 
@@ -141,6 +401,7 @@
 		margin:56px 0 0 0;
 		.v_inner {
 			@include flex;
+			justify-content:space-between;
 			width:1280px;
 			padding-top:24px;
 			margin:0 auto;
@@ -267,6 +528,7 @@
 							padding-top:25px;
 							li {
 								@include flex;
+								margin-bottom:25px;
 								.cm_user {
 									width:40px;
 									height:40px;
@@ -301,6 +563,43 @@
 			}
 			.v_aside {
 				width:402px;
+				.nb_list {
+					ul {
+						li {
+							margin-bottom:8px;
+							a {
+								@include flex;
+								justify-content:space-between;
+								.nb_thumb {
+									margin-right:10px;
+									img {
+										width:168px;
+										height:94px;
+									}
+								}
+								.nb_text {
+									p.nt_1 {
+										display:-webkit-box;
+										-webkit-box-orient:vertical;
+										overflow:hidden;
+										text-overflow:ellipsis;
+										white-space:normal;
+										-webkit-line-clamp:2;
+										font-size:14px;
+									}
+									p.nt_2 {
+										margin:7px 0 3px 0;
+										font-size:13px;
+									}
+									p.nt_3 {
+										font-size:13px;
+									}
+								}
+							}
+							
+						}
+					}
+				}
 			}
 		}
 	}
