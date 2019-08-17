7e38a1d6c1986f9852b609b7f34b9d0b3e8b8953
Kim Taehyeong <tnfbxks55@gmail.com>
2019-08-17 14:58:39 +0900

    youtube_player

---
+++ youtube_player/list.js
@@ -0,0 +1,21 @@
+const music_play_list = [
+        {
+            "title" : "🎉구독자 100만 기념 역대급 레전드🎉 인싸계 최종보스몹 에버랜드 알바 리뷰 1편(feat.아마존 익스프레스)ㅣ워크맨 ep.14",
+            "img": "https://i.ytimg.com/vi/bp-WnzrYzis/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&amp;rs=AOn4CLB9BcTvNiVFwXPviEFSPgtZ_bFXcA",
+            "time" : "2:44",
+            "audio": "./cat1.mp3"
+        },
+        {
+            "title": "EP.50] 술김에 보낸 하룻밤, 그렇게 시작된 연애 [연애의 참견2]",
+            "img": "https://i.ytimg.com/vi/9dE45NFb4B0/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&amp;rs=AOn4CLBXKe0RQBgA41-pcMUzHiEInUeKYA",
+            "time" : "2:44",
+            "audio": "./cat2.mp3"
+        },
+        {
+            "title": "EP.50] 술김에 보낸 하룻밤, 그렇게 시작된 연애 [연애의 참견2]",
+            "img": "https://i.ytimg.com/vi/9dE45NFb4B0/hqdefault.jpg?sqp=-oaymwEYCNIBEHZIVfKriqkDCwgBFQAAiEIYAXAB&amp;rs=AOn4CLBXKe0RQBgA41-pcMUzHiEInUeKYA",
+            "time" : "2:44",
+            "audio": "./cat3.mp3"
+        }
+    ]
+    
